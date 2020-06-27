839c7f4ccc445c06fd68a2183de498821143147f
Sietse <sietse.vanderbom@synetic.nl>
2020-06-27 14:28:45 +0200

    height changed

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 // getting places from APIs
 function loadPlaces(position) {
-  // CORS Proxy to avoid CORS problems
-  const url = 'http://localhost:8080';
-
-  // Foursquare API (limit param: number of maximum places to fetch)
-  const endpoint = `${url}/api/${position.latitude},${position.longitude}`;
+  const endpoint = `http://localhost:8080/api/${position.latitude},${position.longitude}`;
   return fetch(endpoint)
     .then((res) => {
       return res.json()
@@ -23,26 +19,26 @@ window.onload = () => {
   // first get current user location
   return navigator.geolocation.getCurrentPosition(function (position) {
 
-      // than use it to load from remote APIs some places nearby
+      // than use it to load from remote APIs a fourword place nearby
       loadPlaces(position.coords)
         .then(places => {
-          console.log('nou moe: ', places)
-          // places.forEach((place) => {
-          //   const latitude = place.location.lat;
-          //   const longitude = place.location.lng;
-          //
-          //   // add place name
-          //   const placeText = document.createElement('a-link');
-          //   placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-          //   placeText.setAttribute('title', place.name);
-          //   placeText.setAttribute('scale', '15 15 15');
-          //
-          //   placeText.addEventListener('loaded', () => {
-          //     window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-          //   });
-          //
-          //   scene.appendChild(placeText);
-          // });
+          // console.log('nou moe: ', places)
+          places.forEach((place) => {
+            const latitude = place.location.lat;
+            const longitude = place.location.lng;
+
+            // add place name
+            const placeText = document.createElement('a-link');
+            placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+            placeText.setAttribute('title', place.name);
+            placeText.setAttribute('scale', '15 15 15');
+
+            placeText.addEventListener('loaded', () => {
+              window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+            });
+
+            scene.appendChild(placeText);
+          });
         })
     },
     (err) => console.error('Error in retrieving position', err),
